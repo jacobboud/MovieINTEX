f1dc44fff7a60b37a00b5313fbad54d8776a911c
jacobboud <jacobboud2020@gmail.com>
2025-04-07 17:09:48 -0600

    deleted pages

--- frontend/src/types/Project.ts
+++
@@ -1,9 +0,0 @@
-export interface Project {
-  projectID: number;
-  projectName: string;
-  projectType: string;
-  projectRegionalProgram: string;
-  projectImpact: number;
-  projectPhase: string;
-  projectFunctionalityStatus: string;
-}
