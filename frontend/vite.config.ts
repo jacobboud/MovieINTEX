d11d02dae81bf4477587c5bba660d24c074ae071
dallenopenshaw <dallen.openshaw@gmail.com>
2025-04-08 15:11:05 -0600

    csp header and fonts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    headers: {
+      'Content-Security-Policy': 
+        "default-src 'self' https://localhost:5000; " +
+        "font-src 'self' https://fonts.gstatic.com https://use.fontawesome.com; " +
+        "script-src 'self' https://localhost:5000; " +
+        "img-src 'self' data: https://localhost:5000; " +
+        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://use.fontawesome.com; " +
+        "frame-ancestors 'none';"
+    },
   },
 });
