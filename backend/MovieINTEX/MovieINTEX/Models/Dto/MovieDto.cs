1957093f58c94abbf38fd5410ea6005bfcf8ffd3
dallenopenshaw <dallen.openshaw@gmail.com>
2025-04-09 17:16:41 -0600

    1700 security with jacob2

--- backend/MovieINTEX/MovieINTEX/Models/Dto/MovieDto.cs
+++ backend/MovieINTEX/MovieINTEX/Models/Dto/MovieDto.cs
@@ -7,15 +7,32 @@ namespace MovieINTEX.Models.Dto
     {
         [JsonPropertyName("show_id")]
         public string ShowId { get; set; }
+
+        [JsonPropertyName("title")]
         public string Title { get; set; }
+
+        [JsonPropertyName("release_year")]
         public int? ReleaseYear { get; set; }
+
+        [JsonPropertyName("description")]
         public string? Description { get; set; }
 
+        [JsonPropertyName("director")]
         public string? Director { get; set; }
+
+        [JsonPropertyName("rating")]
         public string? Rating { get; set; }
+
+        [JsonPropertyName("duration")]
         public string? Duration { get; set; }
+
+        [JsonPropertyName("type")]
         public string? Type { get; set; }
+
+        [JsonPropertyName("cast")]
         public string? Cast { get; set; }
+
+        [JsonPropertyName("country")]
         public string? Country { get; set; }
     }
 
